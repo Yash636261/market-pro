5ff4ce9aa74d0d098f4852bd19ee32bdedcf3128
yash suthar <98970491+Yash636261@users.noreply.github.com>
2023-03-12 17:27:49 +0530

    Delete scratch directory

--- scratch/routes/Projects.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {
-    getAllProjects,getAProject,CreateAProject,UpdateAProject,DeleteAProject
-}=require('../controllers/Projects');
-
-router.route('/').get(getAllProjects).post(CreateAProject);
-router.route('/:id').get(getAProject).patch(UpdateAProject).delete(DeleteAProject);
-module.exports=router;
